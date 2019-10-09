7b1e5ddb19adff286647a7a8ba1256b012d7a010
eric-burel <eb@lebrun-burel.com>
2019-10-09 17:08:07 +0200

    can run pg_restore function

--- load_backup.py
+++ load_backup.py
@@ -2,6 +2,6 @@
 import psycopg2
 import subprocess
 subprocess.run(
-    "pg_restore --verbose --clean --no-acl --no-owner -h localhost -p 5432 -U postgres -P admin123 -d postgres latest.dump",
+    "pg_restore --verbose --clean --no-acl --no-owner -h localhost -p 5432 -U postgres -P admin123 -d postgres ./dumps/*",
     shell=True
 )
