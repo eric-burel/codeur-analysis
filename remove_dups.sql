00e4405b6bccb3ba914bce55357055936e268079
eric-burel <eb@lebrun-burel.com>
2019-12-04 21:46:03 +0100

    better dup removal function

--- remove_dups.sql
+++ remove_dups.sql
@@ -1,4 +1,5 @@
-/* We need a temporary table */
+/* Solution with tmp table*/
+/*
 DROP TABLE  IF EXISTS tmp_scraper_codeurproject;
 CREATE TABLE tmp_scraper_codeurproject as 
 (
@@ -8,3 +9,13 @@ DELETE from scraper_codeurproject;
 INSERT INTO scraper_codeurproject 
 SELECT * from tmp_scraper_codeurproject;
 DROP TABLE tmp_scraper_codeurproject;
+*/
+
+/* solution using ctid (physical row position used as unique id) */
+DELETE FROM scraper_codeurproject a USING (
+      SELECT MIN(ctid) as ctid, id
+        FROM scraper_codeurproject
+        GROUP BY id HAVING COUNT(*) > 1
+      ) b
+      WHERE a.id = b.id
+      AND a.ctid <> b.ctid
